4d0b08d651c5712f73ed495289e0f72c684285b5
Putu.Yogi <youputra@gmail.com>
2023-02-25 20:09:25 +0800

    Add flowbite and update readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx}"],
+  content: [
+    "./src/**/*.{html,js,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
